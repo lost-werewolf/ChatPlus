be5b95f2ebe28f9675cd3479880f9f234609c668
lost-werewolf <sebmartinpersonal@gmail.com>
2025-10-08 09:19:44 -0400

    Re-Implement ChatManager.AddChatText
    ChatManager.AddChatText will now add the text at the cursor position rather than adding to the end of the string

--- HandleChatDetourSystem.cs
+++ HandleChatDetourSystem.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Terraria;
 using Terraria.ModLoader;
 using Terraria.UI.Chat;
 
@@ -24,11 +25,18 @@ namespace ChatPlus.Core.Chat
             On_ChatManager.AddChatText -= OverrideAddChatText;
         }
 
+        
+        // Because this method usually just adds to Main.chatText, we're replacing it entirely
+        // This version will insert the text at the caretPos in Main.chatText.
         private bool OverrideAddChatText(On_ChatManager.orig_AddChatText orig, ReLogic.Graphics.DynamicSpriteFont font, string text, Vector2 baseScale)
         {
-            if (!orig.Invoke(font, text, baseScale)) return false;
+            int safeWidth = Main.screenWidth - 330;
+            if (ChatManager.GetStringSize(font, Main.chatText + text, baseScale).X > safeWidth) return false;
 
-            // Move the caret after we've gone ahead and added the text
+            // Safety check, just in case, since inserting text could possibly insert at an out-of-bounds spot in the string
+            HandleChatSystem.caretPos = Math.Clamp(HandleChatSystem.caretPos, 0, Main.chatText.Length);
+
+            Main.chatText = Main.chatText.Insert(HandleChatSystem.caretPos, text);
             HandleChatSystem.caretPos += text.Length;
             return true;
         }
